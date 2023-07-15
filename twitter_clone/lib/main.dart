e8ab5711307ad81b7cb84fe04f7bc1255d47ed1b
Matheus <plutaozaum@gmail.com>
2023-07-14 23:06:20 -0300

    center and space texts

--- twitter_clone/lib/main.dart
+++ twitter_clone/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -12,57 +12,77 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: true,
-        home: Scaffold(
-          backgroundColor: Color(0xff1DA1F2),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.network(
-                  "https://www.iconsdb.com/icons/preview/white/twitter-xxl.png",
-                  height: 66,
-                  width: 80,
-                ),
-                Text("Welcome to Twitter. What'd you like to do?",
-                    style: TextStyle(
-                        color: Colors.white, fontSize: 30, fontFamily: "Sora")),
-                SizedBox(
-                  height: 30,
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text("Sign in!"),
-                    style: TextButton.styleFrom(
-                      textStyle: TextStyle(fontFamily: 'Sora', fontSize: 15.0),
-                      padding: EdgeInsets.all(15.0),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      fixedSize: Size.fromWidth(260),
-                      backgroundColor: Colors.white,
-                    )
-                    // foregroundColor: Colors.white),
-                    ),
-                SizedBox(
-                  height: 30,
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text("Sign out!"),
-                    style: TextButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        textStyle: TextStyle(
-                          fontFamily: 'Sora',
-                          fontSize: 15.0,
-                        ),
-                        padding: EdgeInsets.all(15.0),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        fixedSize: Size.fromWidth(260),
-                        backgroundColor: Color(0xff1DA1F2))),
-              ],
+      debugShowCheckedModeBanner: false,
+      title: 'Twitter',
+      theme: ThemeData(
+        primaryColor: Color(0xff1DA1F2),
+        fontFamily: 'Sora',
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff1DA1F2),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              'https://www.iconsdb.com/icons/preview/white/twitter-xxl.png',
+              height: 66,
+              width: 80,
             ),
-          ),
-        ));
+            Padding(
+              padding: EdgeInsets.only(left: 40),
+              child: Text('Welcome to Twitter. What\'d you like to do?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  )),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Sign in!'),
+              style: TextButton.styleFrom(
+                textStyle: TextStyle(fontSize: 15, fontFamily: "Sora"),
+                padding: EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                fixedSize: Size.fromWidth(260),
+                backgroundColor: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Sign out!'),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(fontSize: 15),
+                padding: EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                fixedSize: Size.fromWidth(260),
+                backgroundColor: Color(0xff1DA1F2),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
